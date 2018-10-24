5834d8b2b3bfa8b88c9221920bdb0cb195026c7e
Jochen Probst <jochen.probst@diconium.com>
2018-10-24 11:41:39 +0200

    add support for static files

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {express, app} from '@pubcore/node-server-docker'
 import http404 from './lib/http404'
 import mapRouter from './lib/router'
 import merge from 'merge'
+import fs from 'fs'
 
 export default (config, requireComponent) => {
 	const {components, componentDefault} = config,
@@ -9,6 +10,21 @@ export default (config, requireComponent) => {
 		mapRoutePath = ({context_path}) => ':context_path(' + context_path + ')/?'
 
 	app.use(express.json())
+	packages.forEach(id => {
+		var staticFilesPath = './node_modules/' + id + '/htdocs'
+		try {
+			if (fs.existsSync(staticFilesPath)) {
+				app.use(
+					components[id].context_path,
+					express.static(staticFilesPath)
+				)
+			}
+		} catch(err) {
+			//static files are optional
+			// eslint-disable-next-line no-console
+			console.log(`No static-files support for ${id} ("htdocs" directory not found)`)
+		}
+	})
 
 	if(process.env.NODE_ENV === 'development') {
 		//to prune require.cache on change; load this package only in dev-mode
